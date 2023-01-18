d8ca309aa3d19a2428f95dcf5ee772b5c9b32bb0
Tom <ffrancois737@gmail.com>
2023-01-18 16:19:24 +0800

    cors v2

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const cors = require('cors');
 
 //express app
 const app = express()
-app.use(cors({origin: true}));
+
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 
 //middleware
